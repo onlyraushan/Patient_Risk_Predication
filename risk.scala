d6068748a4164333d1d4e9c5536e32ccaaf1eca4
Raushan Kumar Sahu <145831186+onlyraushan@users.noreply.github.com>
2025-08-26 10:55:12 +0530

    Update risk.scala

--- risk.scala
+++ risk.scala
@@ -10,10 +10,30 @@ object risk{
     connectionProperties.setProperty("password", "211100")
     connectionProperties.setProperty("driver", "org.postgresql.Driver")
 
-    val df: DataFrame = spark.read
-      .jdbc(jdbcUrl, "(SELECT * FROM dim_hospital LIMIT 1000) as blah", connectionProperties)
+    //for reading data from database
+    //val df: DataFrame = spark.read
+     // .jdbc(jdbcUrl, "(SELECT * FROM dim_hospital LIMIT 1000) as blah", connectionProperties)
 
-    df.show()
+    //df.show()
+    val patientDF = spark.read.jdbc(jdbcUrl, "dim_patient", connectionProperties)
+    val hospitalDF = spark.read.jdbc(jdbcUrl, "dim_hospital", connectionProperties)
+    val diagnosisDF = spark.read.jdbc(jdbcUrl, "dim_diagnosis", connectionProperties)
+    val riskDF = spark.read.jdbc(jdbcUrl, "fact_patient_risk", connectionProperties)
+    patientDF.show() // only show the tables
+    hospitalDF.show()
+    diagnosisDF.show()
+    riskDF.show()
+
+    //join tables
+    val fullDF = riskDF
+      .join(patientDF, "patient_id")
+      .join(hospitalDF, "hospital_id")
+      .join(diagnosisDF, "diagnosis_id")
+
+    //Data Cleaning
+    val cleanedDF = fullDF.na.fill("Unknown") // Fill missing strings
+      .na.fill(0) // Fill missing numbers
+      .dropDuplicates()
   }
 }
 
